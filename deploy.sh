28e176ea2c7c3dab20e9a86c4b51e66dc1fbc8ac
Ben Fu <ben@benfu.me>
2020-03-25 12:08:27 -0500

    Add gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
-
 for chart_dir in charts/*/; do
     chart=$(basename $chart_dir)
     echo "Packaging $chart..."
+    set -x
     helm package $chart_dir -d dist/charts/$chart
+    set +x
 done
 
+echo "Creating chart index..."
+set -x
 helm repo index dist/charts
+set +x
 
+echo "Deploying chart to Github Pages..."
+
+set -x
 git add dist/charts
 git commit -m "Update charts"
-git subtree pull --prefix dist/ origin gh-pages --squash
-git subtree push --prefix dist/ origin gh-pages
+git subtree push --prefix dist/charts origin gh-pages
+set +x
